7f60d92587cfc6fb891d3308ea85d711f60dbcdf
Andrzej Dybowski <janjej@hotmail.com>
2017-11-05 20:27:34 +0100

    tworzenie gniazda

--- winsock_client/main.cpp
+++ winsock_client/main.cpp
@@ -6,7 +6,7 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int main() {
+int main(int argc, char* argv[]) {
 
 	/*inicjalizacja gniazda*/
 	WSADATA wsaData;
@@ -16,6 +16,35 @@ int main() {
 		std::cout << "WSAStartup failed: " << iResult << std::endl;
 		return 1;
 	}
+	/*---------------------------*/
+
+	/*tworzenie gniazda*/
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		std::cerr << "getaddrinfo failed: " << iResult << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+	ptr = result;
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		std::cerr << "Error at socket(): " << WSAGetLastError() << std::endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
 	/*---------------------------*/
 	return 0;
 }
